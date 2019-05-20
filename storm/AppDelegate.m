9a5236064efb94f43c483293ea108325053bbf0b
kevin <kevin@kevinZdeMacBook-Pro-2.local>
2019-05-20 15:00:41 +0800

    打包前的状态

--- storm/AppDelegate.m
+++ storm/AppDelegate.m
@@ -117,6 +117,7 @@ static NSString *kStoreKey = @"StoreKey";
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
 }
 
 
@@ -127,6 +128,7 @@ static NSString *kStoreKey = @"StoreKey";
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 
